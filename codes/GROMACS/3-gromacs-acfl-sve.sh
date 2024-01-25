8327bb30750c1a80005375d278c82221466e707f
Shun Utsui <shutsui@amazon.com>
2024-01-26 00:14:06 +0800

    Updating GROMACS job submission script

--- codes/GROMACS/3-gromacs-acfl-sve.sh
+++ codes/GROMACS/3-gromacs-acfl-sve.sh
@@ -7,16 +7,25 @@
 #SBATCH --error=%x_%j.err
 #SBATCH --output=%x_%j.out
 
+export PATH=/shared/tools/openmpi-4.1.5-arml-34/bin:$PATH
+export LD_LIBRARY_PATH=/shared/tools/openmpi-4.1.5-arml-34/lib:$LD_LIBRARY_PATH
 export OMP_NUM_THREADS=1
+
 module use /shared/arm/modulefiles
 module load acfl/23.04.1
 module load armpl/23.04.1
 module load libfabric-aws/1.17.1
 
 # Download test case
-mkdir /shared/gromacsBM && cd /shared/gromacsBM
+
+# Download test case
+ls /shared/gromacsBM
+if [ $? -eq 1 ]; then
+  mkdir /shared/gromacsBM
+fi
+cd /shared/gromacsBM
 wget https://repository.prace-ri.eu/ueabs/GROMACS/2.2/GROMACS_TestCaseA.tar.xz
 tar xf GROMACS_TestCaseA.tar.xz
-
+cd GROMACS_TestCaseA
 
 mpirun -np $SLURM_NTASKS /shared/gromacs2022.5-armcl-sve/bin/gmx_mpi mdrun -s ion_channel.tpr -nsteps 10000
