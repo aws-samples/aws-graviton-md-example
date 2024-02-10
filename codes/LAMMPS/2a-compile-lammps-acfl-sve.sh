c5e03fbd305f76b28d279c8ab5c735366bbe38b9
Nathaniel Ng <107059164+ngnatk@users.noreply.github.com>
2024-02-10 12:34:36 +0800

    Update 2a-compile-lammps-acfl-sve.sh
    fix folders and flags

--- codes/LAMMPS/2a-compile-lammps-acfl-sve.sh
+++ codes/LAMMPS/2a-compile-lammps-acfl-sve.sh
@@ -12,13 +12,12 @@ export OPENMPI_VERSION=4.1.5
 export CC=armclang
 export CXX=armclang++
 export FC=armflang
-export CFLAGS="-mcpu=neoverse-512tvb"
 
 cd ${INSTALLDIR}/lammps/src/MAKE/MACHINES
 target="aarch64_arm_openmpi_armpl"
 echo "${target}"
-sed -i 's/CCFLAGS =\t-O3 -mcpu=native/CCFLAGS =       -O3 -march=armv8-a+sve -fopenmp -mcpu=neoverse-v1 --param=aarch64-autovec-preference=1/g' ./Makefile.${target}
-sed -i 's/LINKFLAGS =\t-g -O/LINKFLAGS =     -g -O -fopenmp/g' ./Makefile.${target}
+cp Makefile.aarch64_g++_openmpi_armpl Makefile.aarch64_g++_openmpi_armpl.bak
+sed -i 's/CCFLAGS =.*/CCFLAGS = -march=armv8-a+sve -Rpass=loop-vectorize/g' ./Makefile.${target}
 cd ../..
 
 make clean-all
@@ -30,6 +29,7 @@ make yes-rigid
 make yes-asphere
 make yes-opt
 make yes-openmp
-make -j $(nproc) mode=shlib mpi 2>&1 | tee lammps-make-mode-shlib-mpi.out
+make -j $(nproc) ${target}
 
-mkdir -p /shared/lammps/armpl-sve && cp ./lmp_mpi /shared/lammps/armpl-sve/lmp_aarch64_arm_openmpi_armpl && cp ./liblammps_mpi.so /shared/lammps/armpl-sve/
+mkdir -p /shared/tools/lammps/armpl-sve
+cp ${INSTALLDIR}/lammps/src/${target} /shared/tools/lammps/armpl-sve
