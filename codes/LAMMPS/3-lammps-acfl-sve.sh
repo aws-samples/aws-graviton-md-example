aa42df74c689e5bffa763d89dd30845d3daf859c
Nathaniel Ng <107059164+ngnatk@users.noreply.github.com>
2024-02-13 15:23:56 +0800

    Update 3-lammps-acfl-sve.sh

--- codes/LAMMPS/3-lammps-acfl-sve.sh
+++ codes/LAMMPS/3-lammps-acfl-sve.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=lj
+#SBATCH --nodes=4
 #SBATCH --partition=queue-1
 #SBATCH --tasks-per-node=64
 #SBATCH --cpus-per-task=1
@@ -8,16 +9,23 @@
 
 ## This is an example job script of a 1 node job using the Lenard Jones input file. 
 
-export PATH=/shared/tools/openmpi-4.1.5-arml-34/bin:$PATH
-export LD_LIBRARY_PATH=/shared/tools/openmpi-4.1.5-arml-34/lib:$LD_LIBRARY_PATH
-export OMP_NUM_THREADS=1
-
 module use /shared/arm/modulefiles
 module load acfl/23.04.1
 module load armpl/23.04.1
 module load libfabric-aws/1.17.1
 
-LAMMPS_BENCH="/shared/lammps/bench"
-cd ${LAMMPS_BENCH}
+export PATH=/shared/tools/openmpi-4.1.5-arml/bin:$PATH
+export LD_LIBRARY_PATH=/shared/tools/openmpi-4.1.5-arml/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/ec2-user/lammps/src/:$LD_LIBRARY_PATH
 
-mpirun -np $SLURM_NTASKS env LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/shared/lammps/armpl-sve/ /shared/lammps/armpl-sve/lmp_aarch64_arm_openmpi_armpl -in in.lj
+export OMP_NUM_THREADS=1
+export OMPI_MCA_mtl_base_verbose=1
+export NX=32
+export NY=32
+export NZ=32
+
+MPIRUN="/shared/tools/openmpi-4.1.5-arml/bin/mpirun"
+LAMMPS_EXE="/home/ec2-user/software/lammps/src/lmp_aarch64_arm_openmpi_armpl"
+N="256"
+
+$MPIRUN -np $N $LAMMPS_EXE -var x $NX -var y $NY -var z $NZ -in in.lj
